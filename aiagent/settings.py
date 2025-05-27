7cb1d2b4b02b6a1d4718c83ae579728e5bd99f49
Mobix <mostafakhorrami950@gmail.com>
2025-05-27 06:59:24 +0330

    Replace mysqlclient with PyMySQL for CPanel compatibility

--- aiagent/settings.py
+++ aiagent/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'users_ai', # اپلیکیشن شما
     'rest_framework_simplejwt',
     # 'corsheaders', # اگر از django-cors-headers استفاده می‌کنید، باید اینجا اضافه شود و در MIDDLEWARE هم
+    'django_extensions',
 ]
 
 REST_FRAMEWORK = {
